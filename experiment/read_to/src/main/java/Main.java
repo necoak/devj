363b6d935285f88bda4a30583d6d93cb1af48077
necoak <kaneko1983g@gmail.com>
2021-05-24 01:40:29 +0900

    java experiment: renameTo modify(fix)

--- experiment/read_to/src/main/java/Main.java
+++ experiment/read_to/src/main/java/Main.java
@@ -15,9 +15,30 @@ public class Main {
             return;
         }
 
-        System.out.println("cp wk/xyz -> wk/xyz.tmp");
+        System.out.println("mv wk/xyz1.tmp -> wk/xyz1.renamed");
         File tmpFile1 = new File("wk/xyz.tmp");
-        try (FileOutputStream outStream = new FileOutputStream(tmpFile1)) {
+        File renameToFile1 = new File("wk/xyz1.renamed");
+        try (FileOutputStream outStream = new FileOutputStream(tmpFile1);) {
+            outStream.write(xyzContent);
+            if(renameToFile1.exists() && !renameToFile1.delete()) {
+                System.out.println("delete failed");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(!tmpFile1.renameTo(renameToFile1)) {
+            try {
+                throw new IOException("[tmpFile1] tmpFile.renameTo(classFile) failed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        System.out.println("cp wk/xyz -> wk/xyz2.tmp");
+        File tmpFile2_1 = new File("wk/xyz.tmp");
+        try (FileOutputStream outStream = new FileOutputStream(tmpFile2_1)) {
             outStream.write(xyzContent);
             new IOException();
 
@@ -25,19 +46,25 @@ public class Main {
 
         }
 
-        System.out.println("mv wk/xyz.tmp -> wk/xyz.renamed");
-        File tmpFile2 = new File("wk/xyz.tmp");
-        File renameToFile = new File("wk/xyz.renamed");
-        try (FileOutputStream outStream = new FileOutputStream(tmpFile2);) {
+        System.out.println("mv wk/xyz2.tmp -> wk/xyz2.renamed");
+        File tmpFile2_2 = new File("wk/xyz.tmp");
+        File renameToFile2_2 = new File("wk/xyz2.renamed");
+        try (FileOutputStream outStream = new FileOutputStream(tmpFile2_2);) {
             outStream.write(xyzContent);
-            if(renameToFile.exists() && !renameToFile.delete()) {
+            if(renameToFile2_2.exists() && !renameToFile2_2.delete()) {
                 System.out.println("delete failed");
-            }
-            if(!tmpFile2.renameTo(renameToFile)) {
-                throw new IOException("tmpFile.renameTo(classFile) failed");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        if(!tmpFile2_2.renameTo(renameToFile2_2)) {
+            try {
+                throw new IOException("[tmpFile2_2] tmpFile.renameTo(classFile) failed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
