6c0e2fa9399cb8393fb536bffbf9faa2a65527f8
necoak <kaneko1983g@gmail.com>
2021-06-24 00:43:44 +0900

    poi_sample: first book (first cell operataion)

--- poi_sample/src/main/java/HelloPoi.java
+++ poi_sample/src/main/java/HelloPoi.java
@@ -1,5 +1,7 @@
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.ss.util.WorkbookUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -13,6 +15,15 @@ public class HelloPoi {
 
         String sheetName = WorkbookUtil.createSafeSheetName("新しいシート");
         Sheet sheet1 = wb.createSheet(sheetName);
+
+        CellReference cr = new CellReference("A1");
+
+        sheet1.createRow(0);
+        sheet1.getRow(0).createCell(0);
+
+        Cell cell = sheet1.getRow(cr.getRow()).getCell(cr.getCol());
+        cell.setCellValue(1234);
+
 
         OutputStream fileOut = new FileOutputStream("sample.xlsx");
 
