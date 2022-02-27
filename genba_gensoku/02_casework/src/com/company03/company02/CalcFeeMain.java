3e03f317253895e00d5488285f479277e40fa972
necoak <kaneko1983g@gmail.com>
2022-02-28 08:10:10 +0900

    third: before refactor

---
+++ genba_gensoku/02_casework/src/com/company03/company02/CalcFeeMain.java
@@ -0,0 +1,12 @@
+package com.company03.company02;
+
+public class CalcFeeMain {
+    public static void main(String[] args) {
+
+        Fee fee01 = new Fee("child");
+        System.out.println(fee01);
+
+        Fee fee02 = new Fee("adult");
+        System.out.println(fee02);
+    }
+}
---
+++ genba_gensoku/02_casework/src/com/company03/company02/Fee.java
@@ -0,0 +1,34 @@
+package com.company03.company02;
+
+public class Fee {
+    private String customerType;
+    private final Integer baseFee = 1000;
+
+    public Fee(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public Integer getFee(){
+        Integer fee = baseFee;
+        if (isChild()){
+            fee = childFee();
+        }
+        return fee;
+    }
+
+    private Boolean isChild() {
+        return this.customerType.equals("child");
+    }
+
+    private Integer childFee() {
+        return (int)Math.round(baseFee * 0.5);
+    }
+
+    @Override
+    public String toString() {
+        return "Fee{" +
+                "customerType='" + customerType + '\'' +
+                ", fee=" + getFee() +
+                '}';
+    }
+}
