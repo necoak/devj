1640f99f01324871f33b4a464b7b40c9dcd4cedd
necoak <kaneko1983g@gmail.com>
2022-02-28 07:54:56 +0900

    second: before refactor

---
+++ genba_gensoku/02_casework/src/com/company02/company/CalcFeeMain.java
@@ -0,0 +1,12 @@
+package com.company02.company;
+
+public class CalcFeeMain {
+    public static void main(String[] args) {
+
+        Fee fee01 = new Fee("child");
+        System.out.println(fee01);
+
+        Fee fee02 = new Fee("adult");
+        System.out.println(fee02);
+    }
+}
---
+++ genba_gensoku/02_casework/src/com/company02/company/Fee.java
@@ -0,0 +1,26 @@
+package com.company02.company;
+
+public class Fee {
+    private String customerType;
+    private final Integer baseFee = 1000;
+
+    public Fee(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public Integer getFee(){
+        Integer fee = baseFee;
+        if (this.customerType.equals("child")) {
+            fee = (int)Math.round(baseFee * 0.5);
+        }
+        return fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Fee{" +
+                "customerType='" + customerType + '\'' +
+                ", fee=" + getFee() +
+                '}';
+    }
+}
