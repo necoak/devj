64b2a97742c24259a7537815526e9034a2a1e595
necoak <kaneko1983g@gmail.com>
2022-03-04 08:17:37 +0900

    とりあえず最低限の画面２個の遷移まで

--- calc_wav_unitprice/src/main/java/com/example/calc_wav_unitprice/MainController.java
+++ calc_wav_unitprice/src/main/java/com/example/calc_wav_unitprice/MainController.java
@@ -3,14 +3,21 @@ package com.example.calc_wav_unitprice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class HelloController {
+public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "hello");
-        return "hello";
+        model.addAttribute("member01", new Member());
+        return "input";
+    }
+
+    @PostMapping("/calc")
+    public String calc(Model model){
+        return "result";
     }
 
 }
---
+++ calc_wav_unitprice/src/main/java/com/example/calc_wav_unitprice/Member.java
@@ -0,0 +1,11 @@
+package com.example.calc_wav_unitprice;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String name;
+    private Integer unitprice;
+    private Integer rawunitprice;
+    private Integer manhour;
+}
