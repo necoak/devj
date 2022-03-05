ca490bae09c415ff0379ad8fb29f9ab9bc925631
necoak <kaneko1983g@gmail.com>
2022-03-05 21:05:17 +0900

    とりあえず最低限の画面２個の遷移まで+Formわたし一部

--- calc_wav_unitprice/src/main/java/com/example/calc_wav_unitprice/MainController.java
+++ calc_wav_unitprice/src/main/java/com/example/calc_wav_unitprice/MainController.java
@@ -3,6 +3,7 @@ package com.example.calc_wav_unitprice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -12,11 +13,13 @@ public class MainController {
     public String index(Model model) {
         model.addAttribute("message", "hello");
         model.addAttribute("member01", new Member());
+
+        model.addAttribute("total_settings", new TotalSettings());
         return "input";
     }
 
     @PostMapping("/calc")
-    public String calc(Model model){
+    public String calc(Model model, @ModelAttribute TotalSettings totalSettings){
         return "result";
     }
 
