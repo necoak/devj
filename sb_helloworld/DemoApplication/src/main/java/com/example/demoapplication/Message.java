f393aa8e883b55f229327238ea7e50f93c15bc75
necoak <kaneko1983g@gmail.com>
2023-03-13 23:16:40 +0900

    spring boot sample JPA

--- sb_helloworld/DemoApplication/src/main/java/com/example/demoapplication/Message.java
+++ sb_helloworld/DemoApplication/src/main/java/com/example/demoapplication/Message.java
@@ -1,9 +1,25 @@
 package com.example.demoapplication;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Table(name = "messages")
+@Entity
 public class Message implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getText() {
         return text;
--- sb_helloworld/DemoApplication/src/main/java/com/example/demoapplication/MessagesController.java
+++ sb_helloworld/DemoApplication/src/main/java/com/example/demoapplication/MessagesController.java
@@ -8,28 +8,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping("messages")
 public class MessagesController {
+
     @Autowired
-    JdbcTemplate jdbcTemplate;
-    final List<Message> messages = new CopyOnWriteArrayList<>();
+    MessageRepository messageRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Message> getMessages(){
-        return jdbcTemplate.query("SELECT text FROM messages ORDER BY id", (rs, i) ->{
-            Message m = new Message();
-            m.setText(rs.getString("text"));
-            return m;
-        });
+        return messageRepository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Message postMessage(@RequestBody Message message){
-        messages.add(message);
-        jdbcTemplate.update("INSERT INTO messages(text) VALUES (?)", message.getText());
-        return message;
+        return messageRepository.save(message);
     }
 }
